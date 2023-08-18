8d60d640c241f80b4fde4b05c81ecbb8f9c81f8f
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-08-18 18:26:26 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # /api/v1/greetings
+  # /api/v1/items/1/reservations
   namespace :api do
     namespace :v1 do
-      resources :reservations
-      resources :items
       resources :users
+      resources :items do 
+        resources :reservations
+      end
     end
   end
 end
