a8e141c98f31d848f4f7c1d2f1211f24c071207d
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-21 06:29:25 +0500

    Remove name validtion from reservation
    Because we have no name attribute present for reservation so far.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,6 @@ class Reservation < ApplicationRecord
   has_many :item_reservations, dependent: :destroy
   has_many :items, through: :item_reservations
 
-  validates :name, presence: true, length: { maximum: 50 } 
   validates :city, presence: true, length: { maximum: 50 } 
   validates :date, presence: true
 end
