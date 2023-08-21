9e8c8fc7abca68c7a27185c62dd3419e277b61f6
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-21 20:06:43 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # /api/v1/items/1/reservations
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :reservations
+      resources :users do 
+        resources :reservations
+      end
       resources :items do 
         resources :reservations
       end
