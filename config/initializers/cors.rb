768dd764b8bd559c48de2e1ffacb5ea7e3d466b0
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-21 23:25:46 +0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001' # Replace with your React app's URL
+    origins '*' # Replace with your React app's URL
    
     resource '/api/v1/*',
       headers: :any,
