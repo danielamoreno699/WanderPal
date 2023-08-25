077152ef133852c2b9cd4864300109323f44bbf2
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-25 14:39:23 +0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # /api/v1/items/1/reservations
   namespace :api do
     namespace :v1 do
